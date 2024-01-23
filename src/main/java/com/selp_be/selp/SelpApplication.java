ecad580352fc81ba4b6528f83ae88ac4a3c72d4c
Limchihyeon <cse.lyc3004@gmail.com>
2024-01-23 19:36:10 +0900

    세팅

---
+++ src/main/java/com/selp_be/selp/SelpApplication.java
@@ -0,0 +1,13 @@
+package com.selp_be.selp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selp_be/selp/SelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selp_be.selp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
